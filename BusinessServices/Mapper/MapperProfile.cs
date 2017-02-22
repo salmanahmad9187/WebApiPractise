e73fc6aab86fd8fd9fea2a9f813e2f563e6357d4
salmanahmad9187 <jazzi_b786@hotmail.com>
2017-02-22 16:45:45 +0500

    mapper

---
+++ BusinessServices/Mapper/MapperProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using DataModel;
+using BusinessEntites;
+
+namespace BusinessServices
+{
+    public class MapperProfile :Profile
+    {
+        protected override void Configure()
+        {
+            CreateMap<User, UserEntity>();
+            CreateMap<Token, TokenEntity>();
+            CreateMap<Product, ProductEntity>();
+        }
+    }
+}
---
+++ BusinessServices/Mapper/ModelMapper.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusinessServices
+{
+    public class ModelMapper
+    {
+        private static IMapper _mapper;
+
+        private static void InitializeMapper()
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<MapperProfile>());
+            _mapper = config.CreateMapper();
+        }
+
+        public static IMapper Mapper
+        {
+            get
+            {
+                if (_mapper == null)
+                {
+                    InitializeMapper();
+                }
+                return _mapper;
+            }
+        }
+    }
+}
