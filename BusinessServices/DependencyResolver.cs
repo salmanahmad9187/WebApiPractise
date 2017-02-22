7de3afbc96a578878464981aa96eadb164e1406f
salmanahmad9187 <jazzi_b786@hotmail.com>
2017-02-22 16:46:32 +0500

    add

---
+++ BusinessServices/DependencyResolver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Resolver;
+using AutoMapper;
+
+namespace BusinessServices
+{
+    [Export(typeof(IComponent))]
+    public class DependencyResolver : IComponent
+    {
+       public void SetUp(IRegisterComponent registerComponent)
+        {
+            registerComponent.RegisterType<IProductServices, ProductServices>();
+            registerComponent.RegisterInstance<IMapper>(ModelMapper.Mapper);
+        }
+    }
+}
---
+++ BusinessServices/ProductServices.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Transactions;
+using System.Threading.Tasks;
+using AutoMapper;
+using DataModel.UnitOfWork;
+using DataModel;
+using BusinessEntites;
+
+namespace BusinessServices
+{
+    public class ProductServices : IProductServices
+    {
+        private readonly UnitOfWork _unitOfWork;
+        private readonly IMapper mapper;
+        public ProductServices(UnitOfWork unitOfWork, IMapper mapper)
+        {
+            this._unitOfWork = unitOfWork;
+            this.mapper = mapper;
+        }
+
+        public ProductEntity GetProductById(int productId)
+        {
+            var product = _unitOfWork.ProductRepository.GetByID(productId);
+            if (product != null)
+            {
+                var productModel = mapper.Map<ProductEntity>(product);
+                return productModel;
+            }
+            return null;
+        }
+
+        public IEnumerable<ProductEntity> GetAllProducts()
+        {
+            var products = _unitOfWork.ProductRepository.GetAll().ToList();
+            if (products.Any())
+            {
+                var productsModel = mapper.Map<List<ProductEntity>>(products);
+                return productsModel;
+            }
+            return null;
+        }
+
+        public int CreateProduct(ProductEntity productEntity)
+        {
+            using (var scope = new TransactionScope())
+            {
+                var product = new Product
+                {
+                    ProductName = productEntity.ProductName
+                };
+                _unitOfWork.ProductRepository.Insert(product);
+                _unitOfWork.Save();
+                scope.Complete();
+                return product.ProductId;
+            }
+        }
+
+        public bool UpdateProduct(int productId, ProductEntity productEntity)
+        {
+            var success = false;
+            if (productEntity != null)
+            {
+                using (var scope = new TransactionScope())
+                {
+                    var product = _unitOfWork.ProductRepository.GetByID(productId);
+                    if (product != null)
+                    {
+                        product.ProductName = productEntity.ProductName;
+                        _unitOfWork.ProductRepository.Update(product);
+                        _unitOfWork.Save();
+                        scope.Complete();
+                        success = true;
+                    }
+                }
+            }
+            return success;
+        }
+
+        public bool DeleteProduct(int productId)
+        {
+            var success = false;
+            if (productId > 0)
+            {
+                using (var scope = new TransactionScope())
+                {
+                    var product = _unitOfWork.ProductRepository.GetByID(productId);
+                    if (product != null)
+                    {
+                        _unitOfWork.ProductRepository.Delete(product);
+                        _unitOfWork.Save();
+                        scope.Complete();
+                        success = true;
+                    }
+                }
+            }
+            return success;
+        }
+    }
+}
